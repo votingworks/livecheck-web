010be2c97c87985436e6bc844d120bd3d8155d1d
Ben Adida <ben@adida.net>
2023-07-08 13:46:12 -0400

    removing old code

--- app.js
+++ app.js
@@ -12,19 +12,6 @@ function stringToByteArray(input) {
   return new Uint8Array(result);
 }
 
-
-/*
-const message = stringToByteArray("Hello!\n");
-const signature = Signature.import("untrusted comment: \nRWQjxsWYC1ei4uz+kFem6eK50EvLxr0rWGN0AkZwsS/EDmgyh9tig7bXJDvHE2PBO5G3Z8KvRi0g4q7yXYWo++4TegKvaG+GMAI=");
-const pubkey = PublicKey.import("untrusted comment: \nRWQjxsWYC1ei4jZusPuwEvJvxgLiR3ex6h60/Q4BbX4cVTWFrDonl58b");
-
-console.log("verify: ", verify(message, signature, pubkey));
-*/
-
-
-import publicKeys from './pubkeys.json';
-console.log(publicKeys);
-
 var video = document.createElement("video");
 var canvasElement = document.getElementById("canvas");
 var successElement = document.getElementById("success");
@@ -34,6 +21,20 @@ var loadingMessage = document.getElementById("loadingMessage");
 var outputContainer = document.getElementById("output");
 var outputMessage = document.getElementById("outputMessage");
 var outputData = document.getElementById("outputData");
+
+function processCodeData(data) {
+    console.log(data);
+    // display success
+    if (result) {
+	successContentElement.innerHTML = "Machine ID: <b>" + signedMachineId + "</b><br>" + "Seconds Ago: <b>" + secondsAgo + "</b><br>Election ID: <tt>" + electionId.substring(0,10) + "</tt>";
+	canvasElement.hidden = true;
+	successElement.hidden = false;
+	continueAnimation = false;
+	outputContainer.hidden = true;
+    } else {
+	console.log("verification failed");
+    }
+}
 
 var continueAnimation = true;
 function tick() {
@@ -52,48 +53,7 @@ function tick() {
 	});
 	
 	if (code) {
-	    var code_url
-	    try {
-		// extract payload and signature
-		code_url = new URL(code.data);
-	    } catch(err) {
-	    }
-
-	    if (code_url) {
-		const {protocol, hostname, pathname, port, searchParams} = code_url;
-		if (protocol != 'https:' || hostname != 'check.voting.works' || pathname != '/' || port != '') {
-		    return;
-		}
-
-		const machineId = searchParams.get("m");
-		const payload = searchParams.get("p");
-		const signature = searchParams.get("s");
-		
-		console.log("got", machineId, payload, signature);
-		
-		if (!machineId || !payload || !signature) {
-		    return;
-		}
-		
-		// verify signature
-		const message = stringToByteArray("lc." + payload);
-		const fullSignature = Signature.import("untrusted comment: \n" + signature);
-		const pubkey = PublicKey.import("untrusted comment: \n" + publicKeys[machineId]);
-
-		const result = verify(message, fullSignature, pubkey);
-		const [signedMachineId, timestamp, electionId] = payload.split('|');
-		const secondsAgo = (new Date().getTime() - parseInt(timestamp)) / 1000;
-		// display success
-		if (result) {
-		    successContentElement.innerHTML = "Machine ID: <b>" + signedMachineId + "</b><br>" + "Seconds Ago: <b>" + secondsAgo + "</b><br>Election ID: <tt>" + electionId.substring(0,10) + "</tt>";
-		    canvasElement.hidden = true;
-		    successElement.hidden = false;
-		    continueAnimation = false;
-		    outputContainer.hidden = true;
-		} else {
-		    console.log("verification failed");
-		}
-	    }
+	    processCodeData(code.data);
 	}
     }
     if (continueAnimation) {
